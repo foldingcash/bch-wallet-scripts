1f96e6ccc9291878867205297e9302fdca4a97af
Miguel Molina Jr. <molim134@gmail.com>
2024-08-11 22:40:02 -0400

    a utility to see my UTXOs from and address

---
+++ index.js
@@ -0,0 +1,47 @@
+import { ElectrumNetworkProvider } from 'cashscript';
+
+import getWallet from './getWallet.js';
+import { promptInt, prompt } from './prompt.js';
+
+import config from './config.json' assert { type: 'json' };
+
+const provider = new ElectrumNetworkProvider(config.Network);
+
+async function showUTXOs(){
+
+    const address = prompt('Address: ');
+    const wallet = await getWallet();
+    const inputs = await provider.getUtxos(address);
+
+    console.log(`Found '${inputs.length}' UTXOs`);
+
+    inputs.forEach((i, index) => {
+        console.log(`Input (${index}): `, i);
+    })
+}
+
+async function main() {
+    let exit = false;
+    do {
+        const menu = 'Menu:\n\t1: Show UTXOs\n\t2: Exit\n\nChoose Selection: ';
+        const response = promptInt(menu); // weird behavior
+        console.log('response', response);
+
+        switch(response) {
+            case 1:
+                console.log('showing utxos');
+                await showUTXOs();
+                break;
+            case 2:
+                exit = true;
+                break;
+        }
+    } while(!exit);
+}
+
+try {
+    await main();
+} catch(error) {
+    console.error('An unhandled exception was thrown: ', error.message);
+    console.error(error);
+}
